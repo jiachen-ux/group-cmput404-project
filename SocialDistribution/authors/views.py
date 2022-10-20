27526230c109c0f4a4d2171ef7eed3d4c5d7b473
Jiachen <jiachen2@ualberta.ca>
2022-10-20 15:27:18 -0600

    user login/register

--- SocialDistribution/authors/views.py
+++ SocialDistribution/authors/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import  render, redirect
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
+from .forms import NewUserForm
+from django.contrib.auth.forms import AuthenticationForm
+
+def login_page(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                # return redirect("authors:homepage") <- this should redirect to a page that show's user's main page
+            else:
+                messages.error(request,"Invalid username or password.")
+        else:
+            messages.error(request,"Invalid username or password.")
+    form = AuthenticationForm()
+    context = {"login_form":form}
+    return render(request, "authors/login.html", context)
+
+
+def register_page(request):
+    if request.method == 'POST':
+        form = NewUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Registration successful." )
+            return redirect('authors/login')
+        messages.error(request, "Unsuccessful registration. Invalid information.")
+
+    form = NewUserForm()
+    context = {'register_form': form}
+
+    return render(request, 'authors/register.html', context)
