e45b336619268295bcbf2539efd7fcbb07d60171
Jiachen <jiachen2@ualberta.ca>
2022-10-20 16:32:41 -0600

    update model: author, follower, follow requests

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -123,8 +123,9 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-# LOGIN_REDIRECT_URL = 'home'
-# LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+REGISTER_URL = 'register'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
