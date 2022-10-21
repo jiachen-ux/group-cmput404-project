2b0c00c7dbe8f9d62aa7e046d813577afbea85c9
Jiachen <jiachen2@ualberta.ca>
2022-10-20 20:06:49 -0600

    fixed error in model

--- SocialDistribution/authors/models.py
+++ SocialDistribution/authors/models.py
@@ -30,10 +30,8 @@ class Author(models.Model):
 
 class Follower(models.Model):
     type = models.CharField(default='followers', max_length=200)
-    user = models.OneToOneField(to=Author,
-                                on_delete=models.CASCADE,
-                                related_name = "username")
-    items = models.ManyToManyField(to=Author, related_name='items', blank=True)
+    user = models.OneToOneField(Author, on_delete=models.CASCADE) #related_name = "username")
+    items = models.ManyToManyField(Author, related_name='items', blank=True)
 
     def __str__(self):
         return self.user.username
@@ -47,12 +45,9 @@ class Follower(models.Model):
 class FollowRequest(models.Model):
     type = models.CharField(default='Follow', max_length=200)
     summary = models.TextField()
-    actor = models.ForeignKey(to=Author,
-                              on_delete=models.CASCADE,
-                              related_name='username')
-    object = models.ForeignKey(to=Author,
-                               on_delete=models.CASCADE,
-                               related_name='username')
+    actor = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='%(class)s_request_sender')
+    object = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='%(class)s_request_receiver')
+    # see https://stackoverflow.com/questions/22538563/django-reverse-accessors-for-foreign-keys-clashing
 
     def __str__(self):
         return self.actor.username
