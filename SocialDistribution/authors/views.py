c11b5de91cce937bc1d5ff5ad90f9f107bad9b07
Baidadan67 <hb2@ualberta.ca>
2022-10-23 12:23:54 -0600

    update the rest framework

--- SocialDistribution/authors/views.py
+++ SocialDistribution/authors/views.py
@@ -4,6 +4,9 @@ from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from .forms import UserRegisterForm
 from django.contrib.auth.forms import AuthenticationForm
+from rest_framework import generics
+from .models import Author 
+from .serializers import AuthorSerializer
 
 def login_page(request):
     if request.method == "POST":
@@ -38,3 +41,9 @@ def register_page(request):
     context = {'register_form': form}
 
     return render(request, 'authors/register.html', context)
+
+
+class AuthorView (generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
