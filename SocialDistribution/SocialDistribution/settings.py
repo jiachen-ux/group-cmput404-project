4fe3675fde1cbd57c8ed66ce741a75ca350ec0f2
shovo4 <shovo@ualberta.ca>
2022-10-23 19:35:36 -0600

    jj

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'authors.apps.AuthorsConfig',
+    'frontend.apps.FrontendConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
