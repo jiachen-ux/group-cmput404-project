6a96c5fc6aad5d1671a1a21187c4f309709c1ac2
Jiachen <jiachen2@ualberta.ca>
2022-10-25 19:13:15 -0600

    separate author and follower app

--- SocialDistribution/authors/views.py
+++ SocialDistribution/authors/views.py
@@ -43,7 +43,13 @@ def register_page(request):
     return render(request, 'authors/register.html', context)
 
 
-class AuthorView (generics.ListAPIView):
+
+# class AuthorView(generics.RetrieveAPIView):
+    
+
+
+class AuthorListView (generics.ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
     
