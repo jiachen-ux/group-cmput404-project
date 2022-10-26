8c6a2bfc742297b9b661d8175b962ee81e303748
Jiachen <jiachen2@ualberta.ca>
2022-10-25 22:09:29 -0600

    update api view for author and authors

--- SocialDistribution/authors/views.py
+++ SocialDistribution/authors/views.py
@@ -1,13 +1,44 @@
 # Create your views here.
-from django.shortcuts import  render, redirect
+from django.shortcuts import  render, redirect, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth import login, authenticate
 from .forms import UserRegisterForm
 from django.contrib.auth.forms import AuthenticationForm
-from rest_framework import generics
+from rest_framework import generics, response, status
+from rest_framework.response import Response
 from .models import Author 
 from .serializers import AuthorSerializer
+from django.contrib.auth.decorators import login_required
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 
+from SocialDistribution.authors import serializers
+
+class AuthorView(generics.RetrieveAPIView):
+    # https://www.django-rest-framework.org/api-guide/generic-views/ for reference
+    serializer_class = AuthorSerializer
+    http_method_names = ['get', 'put']
+    lookup_field = 'userId'
+
+    # Override get_queryset() https://www.django-rest-framework.org/api-guide/generic-views/#get_querysetself
+    def get_queryset(self):
+        id = self.kwargs['userId']
+        return Author.objects.filter(userId=id)
+    
+    
+class AuthorListView (generics.ListAPIView):
+    # get all authors in local server
+    http_method_names = ['get']
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+    
+    def list(self):
+        try:
+            serializer = AuthorSerializer(self.queryset, many=True)
+            return Response({"type": 'authors',"items":serializer.data}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(f"Error: {e}", status=status.HTTP_404_NOT_FOUND)
+    
+    
 def login_page(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
@@ -18,7 +49,7 @@ def login_page(request):
             if user is not None:
                 login(request, user)
                 messages.info(request, f"You are now logged in as {username}.")
-                # return redirect("authors:homepage") <- this should redirect to a page that show's user's main page
+                # return redirect("authors:profile") <- this should redirect to a page that show's user's main page
             else:
                 messages.error(request,"Invalid username or password.")
         else:
@@ -43,13 +74,24 @@ def register_page(request):
     return render(request, 'authors/register.html', context)
 
 
-
-# class AuthorView(generics.RetrieveAPIView):
+@login_required
+def display_author_profile(request, userId):
+    # get author's info
+    author = get_object_or_404(Author, userId = userId)
+    context = {
+        "author":author
+    }
+    return render(request, 'profile.html', context)
+    
     
 
 
-class AuthorListView (generics.ListAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-
+@login_required
+def get_author(request):
+    # assume author exist and user name is correct
+    username = request.GET['username']
+    author = Author.objects.get(username=username)
+    id = author.userId
+    return redirect('author:profile', userId=id)
     
+
