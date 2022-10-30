c19ca990292796c150ea34e017ada2602bfc278a
shovo4 <shovo@ualberta.ca>
2022-10-30 14:37:18 -0600

    backend stuff

--- SocialDistribution/authors/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import AuthorView
-
-urlpatterns = [
-    path('author', AuthorView.as_view()),
-]
---
+++ network/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Follower)
+#admin.site.register(Like)
+#admin.site.register(Saved)
