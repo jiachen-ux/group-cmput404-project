3e990253eb7345d8a995277c2c7e7b4c968a3cc1
Jiachen Xu <jiachen2@ualberta.ca>
2022-11-22 21:17:09 -0700

    add static_root

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'drf_yasg',
     'rest_framework_simplejwt',
@@ -132,6 +133,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'author/media')
 MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
