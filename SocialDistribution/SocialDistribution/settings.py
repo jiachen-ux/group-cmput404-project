994a2451c3bab037c4ee09844ee21504687277a9
Jiachen Xu <jiachen2@ualberta.ca>
2022-11-23 22:11:52 -0700

    change template to template

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'SocialDistribution.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ os.path.join(BASE_DIR,'frontend')],
+        'DIRS': [ os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
