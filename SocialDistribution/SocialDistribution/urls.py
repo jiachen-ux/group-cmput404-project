4a77be023b6cce8f38e84ce5686a280f74017e3e
Jiachen Xu <jiachen2@ualberta.ca>
2022-11-30 19:06:47 -0700

    move some url to soc/url

--- SocialDistribution/SocialDistribution/urls.py
+++ SocialDistribution/SocialDistribution/urls.py
@@ -41,6 +41,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('authors/<authorId>/', author_view.profile, name="author_profile"),
     path('authors/', author_view.display_author, name="allForeignAuthors"),
+    path("logout", views.logoutView, name="logout_url"),
 
     # path("", include("author.urls")),
     path("", include("comment.urls")),
